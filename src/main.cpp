084059f551f5abd0aaed70ba9ac437c04f6f6f6e
karam kim <karamkim88@lguplus.co.kr>
2021-04-20 05:06:47 -0400

    coding..home_robot navi

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,34 @@
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <std_msgs/String.h>
+
+
+//ros acml_pose subscribe status check
+// ros  
+
+enum State{
+	STATE_IDLE,
+	STATE_GOAL,
+	STATE_MONITORING
+};
+
+State my_state;
+
+
+
+void google_assistant_callback(const std_msgs::String::ConstPtr& msg){
+	ROS_INFO("I heard : [%s]", msg->data.c_str());
+
+	if(my_state == STATE_MONITORING){
+		//send goal cancel
+		my_state = STATE_IDLE; 
+	}else if(my_state == STATE_IDLE){
+
+	}
+
+}
+
 
 
 int main(int argc, char** argv){
@@ -10,26 +38,36 @@ int main(int argc, char** argv){
 
 	ros::NodeHandle nh;
 	ros::Publisher ros_move_pub = nh.advertise<geometry_msgs::PoseStamped>("/move_base_simple/goal", 10);
-	ros::Subscriber ros_state_manage = 
+	ros::Subscriber ros_state_manage = nh.subscribe("GA", 10, google_assistant_callback);
 
 	geometry_msgs::PoseStamped goal;
 
-	ros::Rate loop_rate(10);
+	ros::Rate loop_rate(1);
+
+	ros::Time
 
 
 	goal.header.frame_id = "map";
-	goal.header.stamp = ros::Time::now();
-	goal.pose.position.x = -0.4;
-	goal.pose.position.y = -0.4;
+	//goal.header.stamp = ros::Time::now();
+	goal.pose.position.x = -1;
+	goal.pose.position.y = -1;
 	
 
-	ros_move_pub.publish(goal);
+	my_state = STATE_IDLE;
 	
-	ros::Duration(5).sleep();
 	
 	ROS_INFO("publish goal");
 
 	while(ros::ok()){
+
+		if(my_state == STATE_GOAL){
+
+			ros_move_pub.publish(goal);
+		}
+
+		if(my_state == STATE_GOAL && msg == "finish"){
+			my_state = STATE_IDLE;
+		}
 
 		ros::spinOnce();
 	}
